f3a9174c3de479cf521e7bbac7bfe56dfd370641
DioenDJS <negociospacheti@gmail.com>
2022-01-17 00:10:24 -0300

    :art: generate migrate alter table

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,8 +1,16 @@
+import prismaClient from "../prisma";
 
 class CreateMessageService {
     async execute(text: string, user_id: string){
-
+       const message = await prismaClient.message.create({
+            data: {
+                text,
+                user_id
+            }
+       })
     }
 }
 
 export { CreateMessageService};
+
+// tempo de video 1:18:43
